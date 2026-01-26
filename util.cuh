6d6d9b1f3c8cad4cf1d7addedb4f247eee399cac
0x822a5b87 <hangyudu@yeah.net>
2026-01-26 15:31:25 +0800

    add matrix transpose check

--- util.cuh
+++ util.cuh
@@ -3,25 +3,42 @@
 #include <cstdio>
 #include <cstdlib>
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 #define CHECK(call)                                                              \
-{                                                                                \
-    const cudaError_t error = call;                                              \
-    if (error != cudaSuccess)                                                    \
     {                                                                            \
-        fprintf(stderr, ANSI_COLOR_RED "CUDA Error:" ANSI_COLOR_RESET "\n");     \
-        fprintf(stderr, "    File:    %s\n", __FILE__);                          \
-        fprintf(stderr, "    Line:    %d\n", __LINE__);                          \
-        fprintf(stderr, "    Code:    %d\n", error);                             \
-        fprintf(stderr, "    Reason:  %s\n", cudaGetErrorString(error));         \
-        exit(1);                                                                 \
-    }                                                                            \
-}
+        const cudaError_t error = call;                                          \
+        if (error != cudaSuccess)                                                \
+        {                                                                        \
+            fprintf(stderr, ANSI_COLOR_RED "CUDA Error:" ANSI_COLOR_RESET "\n"); \
+            fprintf(stderr, "    File:    %s\n", __FILE__);                      \
+            fprintf(stderr, "    Line:    %d\n", __LINE__);                      \
+            fprintf(stderr, "    Code:    %d\n", error);                         \
+            fprintf(stderr, "    Reason:  %s\n", cudaGetErrorString(error));     \
+            exit(1);                                                             \
+        }                                                                        \
+    }
 
-#define CHECK_LAST_KERNEL_ERROR()                                                \
-{                                                                                \
-    CHECK(cudaGetLastError());                                                   \
-    CHECK(cudaDeviceSynchronize());                                              \
-}
+#define CHECK_LAST_KERNEL_ERROR()       \
+    {                                   \
+        CHECK(cudaGetLastError());      \
+        CHECK(cudaDeviceSynchronize()); \
+    }
+
+#define CHECK_MATRIX_TRANSPOSE(h_in, h_out, nx, ny, errorCount) \
+    do                                                          \
+    {                                                           \
+        for (int _r = 0; _r < (ny); _r++)                       \
+        {                                                       \
+            for (int _c = 0; _c < (nx); _c++)                   \
+            {                                                   \
+                int _idx_in = _r * (nx) + _c;                   \
+                int _idx_out = _c * (ny) + _r;                  \
+                if (h_in[_idx_in] != h_out[_idx_out])           \
+                {                                               \
+                    (errorCount)++;                             \
+                }                                               \
+            }                                                   \
+        }                                                       \
+    } while (0)
